7fc73b4d68c541893e6ad15b38287ccca2a24df3
Reconfort Daniel <reconfortdanny@gmail.com>
2023-07-31 22:29:41 +0200

    Adding Hamburger Menu

--- main.js
+++
@@ -1,8 +0,0 @@
-const MenuSection = document.getElementById("Menu");
-
-const showMenu = () => {
-  MenuSection.style.display = "block";
-};
-const hideMenu = () => {
-  MenuSection.style.display = "none";
-};
