faaf0e5ff8fe1c97662a1ae4f6bdc1a010bba65e
Reconfort Daniel <reconfortdanny@gmail.com>
2023-08-01 00:53:41 +0200

    Add JS linter checker

---
+++ main.js
@@ -0,0 +1,8 @@
+const MenuSection = document.getElementById("Menu");
+
+export const showMenu = () => {
+  MenuSection.style.display = "block";
+};
+export const hideMenu = () => {
+  MenuSection.style.display = "none";
+};
